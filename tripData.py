f77c825699cc056d9bd2f4257eea1b86d32b4605
youdymoo <youdymoo@umich.edu>
2018-06-08 05:26:34 +0800

    csv generated

--- tripData.py
+++ tripData.py
@@ -5,13 +5,11 @@ import urllib.request
 with urllib.request.urlopen('https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/trips.json') as response:
 	tripData = json.loads(response.read().decode('utf-8'))
 
-output = '['
+output = 'lng,lat\n'
 for dataEntry in tripData:
 	for segment in dataEntry['segments']:
 		lng = segment[0]
 		lat = segment[1]
-		new_entry = '{' + "lng: {}, lat: {}".format(lng, lat) + '}'
-		output += new_entry + ','
+		new_entry = '{}, {}'.format(lng, lat) + '\n'
 output = output[:-1]
-output += ']'
 print(output)
